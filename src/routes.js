cc761b1e73bd7f4f2b8dfa2df61e5e27cb0ed62c
Gustavo <gustavolaranjeiramo@gmail.com>
2021-02-01 13:59:42 -0300

    Ver_0.11

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const multer = require("./app/middlewares/multer");
 const productController = require("./app/controllers/productController");
 const homeController = require("./app/controllers/homeController");
+const searchController = require("./app/controllers/searchController");
 
 /*
 HTTP Verbs
@@ -12,8 +13,13 @@ PUT: Atualiza
 DELETE: Deleta
 */
 
+//Home
 routes.get("/", homeController.index);
 
+//Search
+routes.get("/products/search", searchController.index);
+
+//Products
 routes.get("/products/create", productController.create);
 routes.get("/products/:id", productController.show);
 routes.get("/products/:id/edit", productController.edit);
