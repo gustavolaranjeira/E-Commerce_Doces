5c02ad759cff585610fa2220b04db3c3e0da889e
Gustavo <gustavolaranjeiramo@gmail.com>
2021-02-01 19:38:56 -0300

    Ver_0.11

---
+++ src/routes/users.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const routes = express.Router();
+
+const sessionControler = require("../app/controllers/sessionControler");
+const userControler = require("../app/controllers/userControler");
+
+// //login / logout
+// routes.get("/login", sessionControler.loginForm);
+// routes.post("/login", sessionControler.login);
+// routes.post("/login", sessionControler.logout);
+
+// //reset password / forgot
+// routes.get("/forgot-password", sessionControler.forgotForm);
+// routes.get("/password-reset", sessionControler.resetForm);
+// routes.post("/forgot-password", sessionControler.forgot);
+// routes.post("/password-reset", sessionControler.reset);
+
+// //user register userController
+routes.get("/register", userControler.registerForm);
+// routes.post("/register", userControler.post);
+
+// routes.get("/", userControler.show);
+// routes.put("/", userControler.update);
+// routes.delete("/", userControler.delete);
+
+module.exports = routes;
