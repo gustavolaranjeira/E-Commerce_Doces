ba1041e15b735cf059a50aa46e0e6a3a0f777da1
Gustavo <gustavolaranjeiramo@gmail.com>
2021-02-06 11:09:05 -0300

    Ver_0.1

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -1,153 +1,143 @@
-const { formatPrice, date } = require("../../lib/utils");
+const { formatPrice, date } = require('../../lib/utils');
 
-const Category = require("../models/category");
-const Product = require("../models/product");
-const File = require("../models/file");
+const Category = require('../models/category');
+const Product = require('../models/product');
+const File = require('../models/file');
 
 module.exports = {
-    create(req, res) {
-        //pegar categorias
-        Category.all()
-            .then(function (results) {
-                const categories = results.rows;
-                return res.render("products/create.njk", {
-                    categories,
-                });
-            })
-            .catch(function (err) {
-                throw new Error(err);
-            });
-    },
-    async post(req, res) {
-        //logica de salvar
-        const keys = Object.keys(req.body);
-
-        for (key of keys) {
-            if (req.body[key] == "") {
-                return res.send("Please, fill all fields");
-            }
-        }
-
-        if (req.files.lenght == 0) {
-            return res.send("Please, send at least one image");
-        }
-
-        req.body.user_id = req.session.userId;
-
-        let results = await Product.create(req.body);
-        const productId = results.rows[0].id;
-
-        const filesPromise = req.files.map((file) =>
-            File.create({ ...file, product_id: productId })
-        );
-        await Promise.all(filesPromise);
-
-        return res.redirect(`/products/${productId}`);
-    },
-    async show(req, res) {
-        let results = await Product.find(req.params.id);
-        const product = results.rows[0];
-
-        if (!product) return res.send("Product not found!!");
-
-        const { day, hour, minutes, month } = date(product.updated_at);
-
-        product.published = {
-            day: `${day}/${month}`,
-            hour: `${hour}:${minutes}`,
-        };
-
-        product.oldPrice = formatPrice(product.old_price);
-        product.price = formatPrice(product.price);
-
-        results = await Product.files(product.id);
-        const files = results.rows.map((file) => ({
-            ...file,
-            src: `${req.protocol}://${req.headers.host}${file.path.replace(
-                "public",
-                ""
-            )}`,
-        }));
-
-        return res.render("products/show", { product, files });
-    },
-    async edit(req, res) {
-        let results = await Product.find(req.params.id);
-        const product = results.rows[0];
-
-        if (!product) return res.send("Product not found!!");
-
-        product.old_price = formatPrice(product.old_price);
-        product.price = formatPrice(product.price);
-
-        //get categories
-
-        results = await Category.all();
+  create(req, res) {
+    //pegar categorias
+    Category.all()
+      .then(function (results) {
         const categories = results.rows;
+        return res.render('products/create.njk', {
+          categories,
+        });
+      })
+      .catch(function (err) {
+        throw new Error(err);
+      });
+  },
+  async post(req, res) {
+    //logica de salvar
+    const keys = Object.keys(req.body);
 
-        // get images
-        results = await Product.files(product.id);
-        let files = results.rows;
-        files = files.map((file) => ({
-            ...file,
-            src: `${req.protocol}://${req.headers.host}${file.path.replace(
-                "public",
-                ""
-            )}`,
-        }));
+    for (key of keys) {
+      if (req.body[key] == '') {
+        return res.send('Please, fill all fields');
+      }
+    }
 
-        return res.render("products/edit.njk", { product, categories, files });
-    },
-    async put(req, res) {
-        const keys = Object.keys(req.body);
+    if (req.files.lenght == 0) {
+      return res.send('Please, send at least one image');
+    }
 
-        for (key of keys) {
-            if (req.body[key] == "" && key != "removed_files") {
-                return res.send("Please, fill all fields");
-            }
-        }
+    req.body.user_id = req.session.userId;
 
-        if (req.files.length != 0) {
-            const newFilesPromise = req.files.map((file) =>
-                File.create({ ...file, product_id: req.body.id })
-            );
+    let results = await Product.create(req.body);
+    const productId = results.rows[0].id;
 
-            await Promise.all(newFilesPromise);
-        }
+    const filesPromise = req.files.map((file) => File.create({ ...file, product_id: productId }));
+    await Promise.all(filesPromise);
 
-        if (req.body.removed_files) {
-            //1,2,3,
+    return res.redirect(`/products/${productId}/edit`);
+  },
+  async show(req, res) {
+    let results = await Product.find(req.params.id);
+    const product = results.rows[0];
 
-            // console.log(req.body.removed_files);
+    if (!product) return res.send('Product not found!!');
 
-            const removedFiles = req.body.removed_files.split(","); //1,2,3,
-            // console.log(removedFiles);
+    const { day, hour, minutes, month } = date(product.updated_at);
 
-            const lastIndex = removedFiles.length - 1;
-            // console.log(lakstIndex);
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}:${minutes}`,
+    };
 
-            removedFiles.splice(lastIndex, 1); //1,2,3
-            // console.log(removedFiles.splice(lastIndex, 1));
+    product.oldPrice = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
 
-            const removedFilesPromise = removedFiles.map((id) =>
-                File.delete(id)
-            );
+    results = await Product.files(product.id);
+    const files = results.rows.map((file) => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`,
+    }));
 
-            await Promise.all(removedFilesPromise);
-        }
+    return res.render('products/show', { product, files });
+  },
+  async edit(req, res) {
+    let results = await Product.find(req.params.id);
+    const product = results.rows[0];
 
-        req.body.price = req.body.price.replace(/\D/g, "");
+    if (!product) return res.send('Product not found!!');
 
-        if (req.body.old_price != req.body.price) {
-            const oldProduct = await Product.find(req.body.id);
-            req.body.old_price = oldProduct.rows[0].price;
-        }
-        await Product.update(req.body);
+    product.old_price = formatPrice(product.old_price);
+    product.price = formatPrice(product.price);
 
-        return res.redirect(`/products/${req.body.id}`);
-    },
-    async delete(req, res) {
-        await Product.delete(req.body.id);
-        return res.redirect("products/create");
-    },
+    //get categories
+
+    results = await Category.all();
+    const categories = results.rows;
+
+    // get images
+    results = await Product.files(product.id);
+    let files = results.rows;
+    files = files.map((file) => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`,
+    }));
+
+    return res.render('products/edit.njk', { product, categories, files });
+  },
+  async put(req, res) {
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+      if (req.body[key] == '' && key != 'removed_files') {
+        return res.send('Please, fill all fields');
+      }
+    }
+
+    if (req.files.length != 0) {
+      const newFilesPromise = req.files.map((file) =>
+        File.create({ ...file, product_id: req.body.id })
+      );
+
+      await Promise.all(newFilesPromise);
+    }
+
+    if (req.body.removed_files) {
+      //1,2,3,
+
+      // console.log(req.body.removed_files);
+
+      const removedFiles = req.body.removed_files.split(','); //1,2,3,
+      // console.log(removedFiles);
+
+      const lastIndex = removedFiles.length - 1;
+      // console.log(lakstIndex);
+
+      removedFiles.splice(lastIndex, 1); //1,2,3
+      // console.log(removedFiles.splice(lastIndex, 1));
+
+      const removedFilesPromise = removedFiles.map((id) => File.delete(id));
+
+      await Promise.all(removedFilesPromise);
+    }
+
+    req.body.price = req.body.price.replace(/\D/g, '');
+
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id);
+      req.body.old_price = oldProduct.rows[0].price;
+    }
+    await Product.update(req.body);
+
+    return res.redirect(`/products/${req.body.id}`);
+  },
+  async delete(req, res) {
+    await Product.delete(req.body.id);
+    return res.redirect('products/create');
+  },
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
 const routes = express.Router();
-const multer = require("../app/middlewares/multer");
+const multer = require('../app/middlewares/multer');
 
-const productController = require("../app/controllers/productController");
-const searchController = require("../app/controllers/searchController");
+const productController = require('../app/controllers/productController');
+const searchController = require('../app/controllers/searchController');
 
-const { onlyUsers } = require("../app/middlewares/session");
+const { onlyUsers } = require('../app/middlewares/session');
 
 //Search
-routes.get("/search", searchController.index);
+routes.get('/search', searchController.index);
 
 //Products
-routes.get("/create", onlyUsers, productController.create);
-routes.get("/:id", productController.show);
-routes.get("/:id/edit", onlyUsers, productController.edit);
+routes.get('/create', onlyUsers, productController.create);
+routes.get('/:id/', productController.show);
+routes.get('/:id/edit', onlyUsers, productController.edit);
 
-routes.post("/", onlyUsers, multer.array("photos", 6), productController.post);
-routes.put("/", onlyUsers, multer.array("photos", 6), productController.put);
+routes.post('/', onlyUsers, multer.array('photos', 6), productController.post);
+routes.put('/', onlyUsers, multer.array('photos', 6), productController.put);
 // routes.delete("/", onlyUsers, productController.delete);
 
 module.exports = routes;
